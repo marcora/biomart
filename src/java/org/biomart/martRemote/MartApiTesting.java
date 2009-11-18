aea8b16a46549af9188a9d1240df9c39e167a040
acros <acros@8dd1c8d6-c59f-4bb3-b62a-300035dbf9e7>
2009-11-18 21:53:05 +0000

    - added some exception handling
    git-svn-id: https://code.oicr.on.ca/svn/biomart/biomart-java@696 8dd1c8d6-c59f-4bb3-b62a-300035dbf9e7

--- src/java/org/biomart/martRemote/MartApiTesting.java
+++ src/java/org/biomart/martRemote/MartApiTesting.java
@@ -2,6 +2,8 @@ package org.biomart.martRemote;
 
 import net.sf.json.JSONObject;
 
+import org.biomart.common.general.exceptions.FunctionalException;
+import org.biomart.common.general.exceptions.TechnicalException;
 import org.biomart.common.general.utils.MyUtils;
 import org.biomart.common.general.utils.XmlUtils;
 import org.biomart.martRemote.objects.response.GetRegistryResponse;
@@ -14,67 +16,76 @@ public class MartApiTesting {
 	/**
 	 * quick test/example method
 	 */
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		
-		@SuppressWarnings("unused")
-		MartRegistry dummyMartRegistry = DummyPortal.createDummyMartRegistry();
+		System.out.println("start.");
 		
-		// Instantiate API
-		System.out.println("Loading registry...");
-		MartApi martApi = 
-			new MartApi();	// will use the serialized portal
-			//new MartApi(dummyMartRegistry);	// will take any MartRegistry object
-		System.out.println();
-		
-		// For instance obtain the registry (= list of mats)
-		System.out.println("Getting registry...");
-		GetRegistryResponse getRegistryResponse = martApi.getRegistry("anonymous", "", "xml");
-		System.out.println();
-		
-		// Create the JSON object corresonding to the previous request
-		JSONObject jsonObject = getRegistryResponse.getJsonObject();
-		System.out.println("JSON:");
-		System.out.println(jsonObject);
-		System.out.println();
-		
-		// Create the XML document corresonding to the previous request
-		Document xmlDocument = getRegistryResponse.getXmlDocument();
-		System.out.println("XML:");
-		System.out.println(XmlUtils.getXmlDocumentString(xmlDocument));
-		System.out.println();
-		
-		// Other calls
-		System.out.println("Getting datasets...");
-		System.out.println(MyUtils.capString(
-				martApi.getDatasets("anonymous", "", null, "ensembl_mart_55", 55).getJsonObject().toString()));
-		System.out.println();
-		
-		/**
-		 * To narrow down to just 3 partitions (takes a while if not)
-		 */
-		String partitionFilterArgument = "main_partition_filter.\"hsapiens_gene_ensembl,mmusculus_gene_ensembl,celegans_gene_ensembl\"";
-		
-		System.out.println("Getting root container...");
-		System.out.println(MyUtils.capString(
-				martApi.getRootContainer("anonymous", "", null, "gene_ensembl", partitionFilterArgument).getJsonObject().toString()));
-		System.out.println();
-		
-		System.out.println("Getting attributes...");
-		System.out.println(MyUtils.capString(
-				martApi.getAttributes("anonymous", "", null, "gene_ensembl", partitionFilterArgument).getJsonObject().toString()));
-		System.out.println();
-		
-		System.out.println("Getting filters...");
-		System.out.println(MyUtils.capString(
-				martApi.getFilters("anonymous", "", null, "gene_ensembl", partitionFilterArgument).getJsonObject().toString()));
-		System.out.println();
-		
-		System.out.println("Querying...");
-		System.out.println(MyUtils.capString(
-				martApi.query("anonymous", "", null, 
-				MyUtils.wrappedGetProperty(MartRemoteConstants.QUERY_TEST_PROPERTIES_FILE_PATH_AND_NAME, "query1")
-				).getJsonObject().toString()));
-		System.out.println();
+		try {
+			@SuppressWarnings("unused")
+			MartRegistry dummyMartRegistry = DummyPortal.createDummyMartRegistry();
+			
+			// Instantiate API
+			System.out.println("Loading registry...");
+			MartApi martApi = 
+				new MartApi();	// will use the serialized portal
+				//new MartApi(dummyMartRegistry);	// will take any MartRegistry object
+			System.out.println();
+			
+			// For instance obtain the registry (= list of mats)
+			System.out.println("Getting registry...");
+			GetRegistryResponse getRegistryResponse = martApi.getRegistry("anonymous", "", "xml");
+			System.out.println();
+			
+			// Create the JSON object corresonding to the previous request
+			JSONObject jsonObject = getRegistryResponse.getJsonObject();
+			System.out.println("JSON:");
+			System.out.println(jsonObject);
+			System.out.println();
+			
+			// Create the XML document corresonding to the previous request
+			Document xmlDocument = getRegistryResponse.getXmlDocument();
+			System.out.println("XML:");
+			System.out.println(XmlUtils.getXmlDocumentString(xmlDocument));
+			System.out.println();
+			
+			// Other calls
+			System.out.println("Getting datasets...");
+			System.out.println(MyUtils.capString(
+					martApi.getDatasets("anonymous", "", null, "ensembl_mart_55", 55).getJsonObject().toString()));
+			System.out.println();
+			
+			/**
+			 * To narrow down to just 3 partitions (takes a while if not)
+			 */
+			String partitionFilterArgument = "main_partition_filter.\"hsapiens_gene_ensembl,mmusculus_gene_ensembl,celegans_gene_ensembl\"";
+			
+			System.out.println("Getting root container...");
+			System.out.println(MyUtils.capString(
+					martApi.getRootContainer("anonymous", "", null, "gene_ensembl", partitionFilterArgument).getJsonObject().toString()));
+			System.out.println();
+			
+			System.out.println("Getting attributes...");
+			System.out.println(MyUtils.capString(
+					martApi.getAttributes("anonymous", "", null, "gene_ensembl", partitionFilterArgument).getJsonObject().toString()));
+			System.out.println();
+			
+			System.out.println("Getting filters...");
+			System.out.println(MyUtils.capString(
+					martApi.getFilters("anonymous", "", null, "gene_ensembl", partitionFilterArgument).getJsonObject().toString()));
+			System.out.println();
+			
+			System.out.println("Querying...");
+			System.out.println(MyUtils.capString(
+					martApi.query("anonymous", "", null, 
+					MyUtils.wrappedGetProperty(MartRemoteConstants.QUERY_TEST_PROPERTIES_FILE_PATH_AND_NAME, "query1")
+					).getJsonObject().toString()));
+			System.out.println();
+			
+		} catch (FunctionalException e) {		// usually an error in arguments, or in the algorithm (message should describe the problem...)
+			e.printStackTrace();
+		} catch (TechnicalException e) {		// errors we don't control (connection lost, no more disk space...)
+			e.printStackTrace();
+		}
 		
 		System.out.println("done.");
 	}
